80d0b7746c937d3951032e39ecdd1a5dc036c993
KuersatAydinli <kuersat.aydinli@uzh.ch>
2017-01-26 12:43:25 +0100

    test case for extracting sample size candidates

--- test/SampleSizeExtractorTest/SampleSizeExtractorTest/TestFirstPDF.scala
+++ test/SampleSizeExtractorTest/SampleSizeExtractorTest/TestFirstPDF.scala
@@ -1,12 +1,41 @@
 package SampleSizeExtractorTest.SampleSizeExtractorTest
+import java.io.{File, PrintWriter}
+
+import helper.pdfpreprocessing.pdf.PDFTextExtractor
 import helper.statcheck.{Statchecker => StatChecker}
-import org.scalatest.{FunSuite, Matchers}
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.scalatest.FunSuite
+
 /**
   * Created by Aydinli on 26.01.2017.
   */
-class TestFirstPDF extends FunSuite with Matchers{
-  test("assertion testing"){
-    val x = 3
-    assert(x == 3)
+class TestFirstPDF extends FunSuite{
+
+  test("GetSampleSize"){
+    val paperDir = "test/TestPDFs/manuscript4.pdf"
+    val pdfDoc = PDDocument.load(new File(paperDir))
+    val pdfText = convertPDFtoText(paperDir)
+    assert(pdfText.length == 38)
+
+    val statChecker = StatChecker
+    val sampleSize = statChecker.extractSampleSize(pdfText)
+
+    if (!new File("test/TestPDFs/sampleSizeString" + ".text").exists()) {
+      val pw = new PrintWriter(new File("test/TestPDFs/sampleSizeString" + ".txt"))
+      pw.write(sampleSize)
+      pw.close()
+    }
+    assert(sampleSize.length > 1)
+  }
+
+  def convertPDFtoText(path: String): List[String] = {
+    val paperLink = path
+    val text = new PDFTextExtractor(paperLink).pages
+    if (!new File(paperLink + ".text").exists()) {
+      val pw = new PrintWriter(new File(paperLink + ".txt"))
+      pw.write(text.map(_.toLowerCase()).mkString("\n\n"))
+      pw.close()
+    }
+    text
   }
 }
