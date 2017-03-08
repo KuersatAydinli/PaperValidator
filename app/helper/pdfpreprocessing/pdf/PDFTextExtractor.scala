06216af06a5c4de48128171d24b6e79a14953593
KuersatAydinli <kuersat.aydinli@uzh.ch>
2017-03-08 22:13:24 +0100

    TextExtractor weired skipping of try block - cancling this try

--- app/helper/pdfpreprocessing/pdf/PDFTextExtractor.scala
+++ app/helper/pdfpreprocessing/pdf/PDFTextExtractor.scala
@@ -14,14 +14,12 @@ class PDFTextExtractor(pdfPath: String) extends LazyLogging {
   lazy val pages: List[String] = {
     try {
       val pdDoc: PDDocument = PDDocument.load(new File(pdfPath))
-
       val pdfHighlight: TextHighlight = new TextHighlight("UTF-8")
       pdfHighlight.setLineSeparator(" ")
       pdfHighlight.initialize(pdDoc)
 
-      var txt = List[String]()
-      var pdfText = new StringBuilder
-      for (i <- 0 to pdDoc.getNumberOfPages){
+      var tempList = scala.collection.mutable.ListBuffer.empty[String]
+      for (i <- 0 to pdDoc.getNumberOfPages+1){
         val page: PDPage = pdDoc.getDocumentCatalog.getPages.get(i)
         val region: Rectangle2D = new Rectangle2D.Double(0,20,page.getMediaBox.getWidth,page.getMediaBox.getHeight-80)
         val regionName: String = "content"
@@ -29,12 +27,21 @@ class PDFTextExtractor(pdfPath: String) extends LazyLogging {
         stripper.addRegion(regionName,region)
         stripper.extractRegions(page)
         val currentPage = stripper.getTextForRegion("content")
-        pdfText ++= currentPage
+        tempList += currentPage
       }
-
-//      val txt: List[String] = (0 to pdDoc.getNumberOfPages).map(pdfHighlight.textCache.getText(_)).toList
       pdDoc.close()
-      txt
+      tempList.toList
+//      val txt: List[String] = (0 to pdDoc.getNumberOfPages).map(pdfHighlight.textCache.getText(_)).toList
+//      val txt: List[String] = (0 to pdDoc.getNumberOfPages+1).map(x => {
+//        var tempList = mutable.MutableList[String]
+//        val page: PDPage = pdDoc.getDocumentCatalog.getPages.get(x)
+//        val region: Rectangle2D = new Rectangle2D.Double(0,20,page.getMediaBox.getWidth,page.getMediaBox.getHeight-80)
+//        val regionName: String = "content"
+//        val stripper: PDFTextStripperByArea = new PDFTextStripperByArea()
+//        stripper.addRegion(regionName,region)
+//        stripper.extractRegions(page)
+//        tempList += stripper.getTextForRegion("content")
+//      }).toList
     } catch {
       case e1: Throwable => {
         logger.error("An error occurred while extracting text from pdf ", e1)
